1fe4cd218b76495b1c25613f7386e919d0a01429
Dalal <mu@mu.com>
2019-10-02 17:21:44 +0300

    use ejs

---
+++ express/ejs_cars_and_cats/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.get('/', (request, response) => {
+   response.send("Hello Express");
+
+});
+app.listen(8000, () => console.log("listening on port 8000"));
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.get("/cars", (req, res) => {
+   res.render('cars');
+})
+
+app.get("/cats", (req, res) => {
+   res.render('cats');
+})
+
+app.get("/cars/new", (req, res) => {
+   res.render('form');
+})
