e239fff630a3609a7f6541ab4271634be9d63eef
Dalal <dalalmansourh3@gmail.com>
2019-10-19 22:24:12 +0300

    angular

---
+++ angular/hello_angular/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tasks', {useNewUrlParser: true});
+
+
+app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(express.urlencoded({extended: true}));
+
+
+
+// models
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true, minlength: 2},
+  description: { type: String, required: true, minlength: 5},
+  completed: { type: Boolean, required: true, minlength: 2}
+ },{timestamps: true })
+ // create an object to that contains methods for mongoose to interface with MongoDB
+const Task = mongoose.model('Task', taskSchema);
+
+app.get("/tasks", (req,res) => {
+  Task.find() 
+  .then(data => res.json(data))
+  .catch(err => res.json(err));
+});
+
+
+app.get("/tasks/:id", (req,res) => {
+  Task.find({_id: req.params.id}) 
+  .then(data => res.json(data))
+  .catch(err => res.json(err));
+});
+
+
+
+app.post('/tasks', (req, res) => {
+  var task = new Task(req.body)
+  task.save(function (err, task) {
+      if (err) {
+          console.log("error", err);
+          res.json(err);
+
+      } else {
+          res.json(task)
+      }
+  })
+})
+
+app.put("/tasks/:id", (req,res) => {
+
+  Task.updateOne(function (err, task) {
+    if (err) {
+        console.log("error", err);
+        res.json(err);
+
+    } else {
+        res.json(task)
+    }
+})
+});
+
+
+app.delete("/tasks/:id", (req,res) => {
+  Task.remove({name: req.params.id})
+    .then(deletedTask => {
+      console.log('task deleted:',deletedTask)
+      res.json({msg:"successfuly deleted"})
+    })
+    .catch(err => res.json(err));
+});
+app.listen(8000, () => console.log("listening on port 8000"));
